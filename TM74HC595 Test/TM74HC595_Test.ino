6e76725e5f82f6395b711aac16f4a49d4f435944
Ace-Radom <104905842+Ace-Radom@users.noreply.github.com>
2022-05-05 08:50:53 +0200

    Add files via upload

---
+++ TM74HC595 Test/TM74HC595_Test.ino
@@ -0,0 +1,18 @@
+#include<TM74HC595_Driver.h>
+
+TM74HC595 Display( 9 , 10 , 11 );
+
+void setup(){
+}
+
+void loop(){
+    Display.send( Display.Character_Point( '5' ) , 1 );
+    delay( 1000 );
+    Display.send( Display.Character_Point( '7' ) , 2 , _With_decimalPoint );
+    delay( 1000 );
+    Display.continuesend( Display.Character_Point( '6' ) , 3 , 500 );
+    Display.continuesend( Display.Character_Point( '8' ) , 4 , _With_decimalPoint , 500 );
+    Display.continuesend( "7890" , 500 );
+    bool b[] = { true , false , true , false };
+    Display.continuesend( "3579" , b , 500 );
+}
